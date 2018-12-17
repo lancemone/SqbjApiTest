8e4504d4649872809ef8ad543b5d06b1934d3555
motao <motao0011@gmail.com>
2018-12-17 19:36:11 +0800

    1.0

--- common/path_conf.py
+++ common/path_conf.py
@@ -68,7 +68,7 @@ class PathConf:
 
     def jenkins_allure_path(self):
         rep_path = os.path.split(os.path.abspath(__file__))[0]
-        report_path = os.path.join(os.path.split(rep_path)[0], "allure-report")
+        report_path = os.path.join(os.path.split(rep_path)[0], "allure-results")
         # print(report_path)
         if os.path.exists(report_path):
             return report_path
